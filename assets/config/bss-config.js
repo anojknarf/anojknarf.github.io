775357dacad39e70d847e3bb3af44a0af084c586
jonafrankbrimma <jona.frank@brimmatech.com>
2018-01-18 18:48:19 +0530

    Updated

--- assets/config/bss-config.js
+++ assets/config/bss-config.js
@@ -1,4 +1,4 @@
-var BASE_URL = "http://enigmatic-dawn-99833.herokuapp.com";
+var BASE_URL = "https://enigmatic-dawn-99833.herokuapp.com";
 // var BASE_URL = "https://bluesage-dev.bluesageusa.com/";
 var BUILD_VERSION = "0.8";
 var BUILD_NUMBER = "<BUILD_NUMBER>";
